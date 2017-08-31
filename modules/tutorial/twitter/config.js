b96ac1337f71d56230cebda289ed3cff2bb5c57f
yang lin <linyang@weidian.com>
2017-08-31 22:11:21 +0800

    clear

--- modules/tutorial/twitter/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    config : {
-        level : 8,
-        absolute : true,
-        background : "#fff",
-        source: "modules/demo-app/twitter",
-        animation : "zoom"
-    }
-}
